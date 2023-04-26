5f8480bc163e2e1ea33e13d4f354d9a8f12a78ef
Juan Diego Teheran Olmos <jdteheran@uninorte.edu.co>
2023-04-25 20:08:04 -0500

    fundamentos react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+class Fruta extends React.Component {
+
+
+
+  constructor(props) {
+    super()
+
+    this.state = {
+      cantidad: 0,
+      total: 0
+    }
+
+    console.log('me ejecuté constructor');
+
+  }
+  shouldComponentUpdate() {
+    if (this.state.cantidad < 0) return false
+    return true
+  }
+
+
+
+
+  componentDidMount() {
+    console.log('me ejecuté componentDidMount');
+  }
+
+  async agregar() {
+    await this.setState({
+      cantidad: this.state.cantidad + 1
+    })
+    console.log(this.state);
+  }
+
+  async quitar() {
+      await this.setState({
+        cantidad: this.state.cantidad - 1
+      })
+
+    console.log(this.state);
+  }
+
+
+  render() {
+    console.log('me ejecuté render');
+    return (
+      <div>
+        <h2>Nombre:{this.props.name}</h2>
+        <h3>Precio:{this.props.price}</h3>
+
+        <button onClick={this.agregar.bind(this)}>+</button>
+        <button onClick={this.quitar.bind(this)}>-</button>
+
+        <p>Cantidad:{this.state.cantidad}</p>
+        <p>Total:{this.state.cantidad*this.props.price}</p>
+
+        <hr />
+      </div>
+    )
+  }
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <>
+      <Fruta name={'Fresa'} price={500} />
+      
+    </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
